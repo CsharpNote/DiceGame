e9166103e712bb3cad1483fa5d4893ac621b2b30
EASJ <EASJ@EASJ3212>
2016-09-28 21:49:22 +0200

    Add project files.

---
+++ DiceGame/DiceCup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceGame
+{
+    class DiceCup
+    {
+        private Die die1;
+        private Die die2;
+
+        public DiceCup()
+        {
+            // VERY IMPORTANT! When we have instance fields of a class type,
+            // we MUST remember to create actual objects in the constructor,
+            // that the instance fields can then point to.
+            die1 = new Die();
+            die2 = new Die();
+        }
+
+        // You must create a method with the below header:
+        // public void RollDice()
+
+        // You must create a method with the below header:
+        // public int GetTotalValue()
+
+        // You must create a method with the below header:
+        // public bool IsTotalValueLargerThan(int value)
+    }
+}
---
+++ DiceGame/InsertCodeHere.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceGame
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
