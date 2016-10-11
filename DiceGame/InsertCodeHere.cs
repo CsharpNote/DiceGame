ba1f8725a4a723cf8239bc17441a298eb2e2535b
Martin Kierkegaard <mark@easj.dk>
2016-10-11 10:59:02 +0200

    oprettet dicecup object og kalder metoder påå objektet

--- DiceGame/InsertCodeHere.cs
+++ DiceGame/InsertCodeHere.cs
@@ -10,16 +10,26 @@ namespace DiceGame
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
-            Die die1 = new Die();
-            Console.WriteLine($"the value of the dice is {die1.GetValue()}");
-            die1.RollDie();
-            Console.WriteLine($"the value of the dice is {die1.GetValue()}");
-            die1.RollDie();
-            Console.WriteLine($"the value of the dice is {die1.GetValue()}");
-            die1.RollDie();
-            Console.WriteLine($"the value of the dice is {die1.GetValue()}");
-            die1.RollDie();
-            Console.WriteLine($"the value of the dice is {die1.GetValue()}");
+            //Die die1 = new Die();
+            //Console.WriteLine($"the value of the dice is {die1.GetValue()}");
+            //die1.RollDie();
+            //Console.WriteLine($"the value of the dice is {die1.GetValue()}");
+            //die1.RollDie();
+            //Console.WriteLine($"the value of the dice is {die1.GetValue()}");
+            //die1.RollDie();
+            //Console.WriteLine($"the value of the dice is {die1.GetValue()}");
+            //die1.RollDie();
+            //Console.WriteLine($"the value of the dice is {die1.GetValue()}");
+
+            Console.WriteLine("Dicecup");
+            DiceCup diceCup = new DiceCup();
+            diceCup.RollAllDiceInCup();
+            Console.WriteLine($"Samlet værdi {diceCup.GetTotalValue()}");
+
+            int value1 = 10;
+            bool resultat = diceCup.IsTotalValueLargerThan(value1);
+
+            Console.WriteLine($"Er samlet terninge værdi større end {value1} resutat:{resultat}");
 
 
             Console.ReadLine();
